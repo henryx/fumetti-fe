2bf37d5e269a062717cc66292775befc6dfdb2b5
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-05-05 00:02:59 +0200

    Added title in page

--- src/main/java/com/application/fumetti/views/editors/EditorsView.java
+++ src/main/java/com/application/fumetti/views/editors/EditorsView.java
@@ -1,12 +1,24 @@
 package com.application.fumetti.views.editors;
 
+import com.application.fumetti.Configuration;
 import com.application.fumetti.views.MainLayout;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.quarkus.annotation.VaadinServiceScoped;
 
 @PageTitle("Editori")
 @Route(value = "editors", layout = MainLayout.class)
-public class EditorsView extends HorizontalLayout {
+public class EditorsView extends VerticalLayout {
+    private final Configuration config;
 
+    @VaadinServiceScoped
+    public EditorsView(Configuration config) {
+        this.config = config;
+
+        H1 title = new H1("Editori");
+        title.setVisible(true);
+        add(title);
+    }
 }
