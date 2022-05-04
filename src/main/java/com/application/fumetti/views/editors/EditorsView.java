098b8aea3382ac9bf80f35a4e683e40e5aaf3f01
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-05-05 00:04:14 +0200

    Added grid used to view data

--- src/main/java/com/application/fumetti/views/editors/EditorsView.java
+++ src/main/java/com/application/fumetti/views/editors/EditorsView.java
@@ -1,12 +1,33 @@
 package com.application.fumetti.views.editors;
 
 import com.application.fumetti.Configuration;
+import com.application.fumetti.mappers.EditorsResponse;
+import com.application.fumetti.mappers.data.EditorResult;
 import com.application.fumetti.views.MainLayout;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.quarkus.annotation.VaadinServiceScoped;
+import org.apache.http.client.HttpResponseException;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 
 @PageTitle("Editori")
 @Route(value = "editors", layout = MainLayout.class)
@@ -19,6 +40,63 @@ public class EditorsView extends VerticalLayout {
 
         H1 title = new H1("Editori");
         title.setVisible(true);
-        add(title);
+
+        Grid<EditorResult> grid = new Grid<>(EditorResult.class, false);
+        grid.addColumn(EditorResult::getName).setHeader("Nome");
+        grid.addColumn(EditorResult::getSite).setHeader("Sede");
+        grid.addColumn(EditorResult::getWebsite).setHeader("Sito web");
+
+        try {
+            var data = this.fetch();
+            grid.setItems(data.getData());
+        } catch (URISyntaxException | IOException | InterruptedException ignored) {
+        }
+
+        add(title, grid);
+    }
+
+    private EditorsResponse fetch() throws URISyntaxException, IOException, InterruptedException {
+        try {
+            var uri = new URI(this.config.proto(),
+                    null, this.config.host(), this.config.port(),
+                    "/editors", null, null);
+
+            var client = HttpClient.newBuilder()
+                    .version(HttpClient.Version.HTTP_1_1)
+                    .connectTimeout(Duration.ofSeconds(10))
+                    .build();
+
+            var request = HttpRequest.newBuilder()
+                    .GET()
+                    .uri(uri)
+                    .setHeader("Accept", "application/json")
+                    .build();
+
+            var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() != 200) {
+                throw new HttpResponseException(response.statusCode(), "Error in connection");
+            }
+
+            return EditorsResponse.map(response.body());
+        } catch (URISyntaxException | InterruptedException | IOException e) {
+            Notification notification = new Notification();
+            notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+
+            Div text = new Div(new Text("Failed to retrieve data: " + e.getMessage()));
+
+            Button closeButton = new Button(new Icon("lumo", "cross"));
+            closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
+            closeButton.getElement().setAttribute("aria-label", "Close");
+            closeButton.addClickListener(event -> notification.close());
+
+            HorizontalLayout layout = new HorizontalLayout(text, closeButton);
+            layout.setAlignItems(Alignment.CENTER);
+
+            notification.add(layout);
+            notification.open();
+            notification.setPosition(Notification.Position.MIDDLE);
+
+            throw e;
+        }
     }
 }
