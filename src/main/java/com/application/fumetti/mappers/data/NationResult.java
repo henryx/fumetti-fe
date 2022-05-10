32be12f39a015307b24a2afc3bbbad684100dccf
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-05-10 18:08:10 +0200

    Added getters and setters for result mapper

--- src/main/java/com/application/fumetti/mappers/data/NationResult.java
+++ src/main/java/com/application/fumetti/mappers/data/NationResult.java
@@ -1,5 +1,6 @@
 package com.application.fumetti.mappers.data;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class NationResult {
@@ -16,4 +17,43 @@ public class NationResult {
     @JsonProperty("currency")
     private CurrencyResult currency;
 
+    @JsonCreator
+    public NationResult() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public NationResult setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public NationResult setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public NationResult setSign(String sign) {
+        this.sign = sign;
+        return this;
+    }
+
+    public CurrencyResult getCurrency() {
+        return currency;
+    }
+
+    public NationResult setCurrency(CurrencyResult currency) {
+        this.currency = currency;
+        return this;
+    }
 }
