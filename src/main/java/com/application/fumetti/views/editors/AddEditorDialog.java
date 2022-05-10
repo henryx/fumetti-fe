6112b5c87ebfb0098e91960cde21f44771668583
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-05-10 18:09:58 +0200

    Added combo box used for select nation

--- src/main/java/com/application/fumetti/views/editors/AddEditorDialog.java
+++ src/main/java/com/application/fumetti/views/editors/AddEditorDialog.java
@@ -1,8 +1,10 @@
 package com.application.fumetti.views.editors;
 
 import com.application.fumetti.Configuration;
+import com.application.fumetti.mappers.data.NationResult;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
@@ -13,6 +15,7 @@ import com.vaadin.flow.component.textfield.TextField;
 public class AddEditorDialog extends Dialog {
 
     private final Configuration config;
+    private NationResult nationSelected;
 
     public AddEditorDialog(Configuration config) {
         this.config = config;
@@ -33,10 +36,14 @@ public class AddEditorDialog extends Dialog {
         var lastNameField = new TextField("Sede", "", "");
         var websiteField = new TextField("Sito Web", "", "");
 
+        var nationsCombo = new ComboBox<NationResult>("Nazione");
+        nationsCombo.setItemLabelGenerator(NationResult::getName);
+        nationsCombo.addValueChangeListener(e -> nationSelected = e.getValue());
+
 
         var layout = new FormLayout();
         layout.setResponsiveSteps(new FormLayout.ResponsiveStep("0", 2));
-        layout.add(firstNameField, lastNameField, websiteField);
+        layout.add(firstNameField, lastNameField, websiteField, nationsCombo);
 
         return layout;
     }
