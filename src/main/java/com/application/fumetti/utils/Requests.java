35c428f2a30332c96e7d3e5b7ca74fba415be3db
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-05-11 11:35:01 +0200

    Removed unused try/catch

--- src/main/java/com/application/fumetti/utils/Requests.java
+++ src/main/java/com/application/fumetti/utils/Requests.java
@@ -25,49 +25,41 @@ public class Requests {
     }
 
     public String get(String path) throws URISyntaxException, IOException, InterruptedException {
-        try {
-            var uri = new URI(this.config.proto(),
-                    null, this.config.host(), this.config.port(),
-                    path, null, null);
+        var uri = new URI(this.config.proto(),
+                null, this.config.host(), this.config.port(),
+                path, null, null);
 
-            var request = HttpRequest.newBuilder()
-                    .GET()
-                    .uri(uri)
-                    .setHeader("Accept", "application/json")
-                    .build();
+        var request = HttpRequest.newBuilder()
+                .GET()
+                .uri(uri)
+                .setHeader("Accept", "application/json")
+                .build();
 
-            var response = this.client.send(request, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode() != 200) {
-                throw new HttpResponseException(response.statusCode(), "Error in connection");
-            }
-
-            return response.body();
-        } catch (URISyntaxException | InterruptedException | IOException e) {
-            throw e;
+        var response = this.client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() != 200) {
+            throw new HttpResponseException(response.statusCode(), "Error in connection");
         }
+
+        return response.body();
     }
 
     public String post(String path, String body) throws URISyntaxException, IOException, InterruptedException {
-        try {
-            var uri = new URI(this.config.proto(),
-                    null, this.config.host(), this.config.port(),
-                    path, null, null);
+        var uri = new URI(this.config.proto(),
+                null, this.config.host(), this.config.port(),
+                path, null, null);
 
-            var request = HttpRequest.newBuilder()
-                    .POST(HttpRequest.BodyPublishers.ofString(body))
-                    .uri(uri)
-                    .setHeader("Content-Type", "application/json")
-                    .setHeader("Accept", "application/json")
-                    .build();
+        var request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .uri(uri)
+                .setHeader("Content-Type", "application/json")
+                .setHeader("Accept", "application/json")
+                .build();
 
-            var response = this.client.send(request, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode() != 200) {
-                throw new HttpResponseException(response.statusCode(), "Error in connection: " + response.body());
-            }
-
-            return response.body();
-        } catch (URISyntaxException | InterruptedException | IOException e) {
-            throw e;
+        var response = this.client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() != 200) {
+            throw new HttpResponseException(response.statusCode(), "Error in connection: " + response.body());
         }
+
+        return response.body();
     }
 }
