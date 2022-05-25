9f23ab6e5445e0f6ce04ec4c5a1e2ecd126f0161
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-05-25 16:20:01 +0200

    Populated grid

--- src/main/java/com/application/fumetti/views/nations/NationsView.java
+++ src/main/java/com/application/fumetti/views/nations/NationsView.java
@@ -1,25 +1,35 @@
 package com.application.fumetti.views.nations;
 
 import com.application.fumetti.Configuration;
+import com.application.fumetti.mappers.Response;
 import com.application.fumetti.mappers.data.NationResult;
+import com.application.fumetti.utils.Notifications;
 import com.application.fumetti.utils.Requests;
 import com.application.fumetti.views.MainLayout;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.quarkus.annotation.VaadinServiceScoped;
 
+import javax.inject.Inject;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 @PageTitle("Nazioni")
 @Route(value = "nations", layout = MainLayout.class)
 public class NationsView extends VerticalLayout {
     private final Configuration config;
     private final Grid<NationResult> grid;
+    private final ObjectMapper mapper;
 
     @VaadinServiceScoped
     public NationsView(Configuration config) {
         this.config = config;
+        this.mapper = new ObjectMapper();
         var req = new Requests(this.config);
 
         var title = new H1("Nazioni");
@@ -30,6 +40,15 @@ public class NationsView extends VerticalLayout {
         this.grid.addColumn(NationResult::getId).setHeader("Id").setVisible(false);
         this.grid.addColumn(NationResult::getName).setHeader("Nome");
         this.grid.addColumn(NationResult::getSign).setHeader("Simbolo");
+
+        try {
+            var body = req.get("/nations");
+            var data = this.mapper.readValue(body, Response.class);
+            this.grid.setItems(data.getData());
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            Notifications.error(e);
+        }
+        this.grid.addThemeVariants(GridVariant.LUMO_WRAP_CELL_CONTENT);
 
         add(title, this.grid);
     }
