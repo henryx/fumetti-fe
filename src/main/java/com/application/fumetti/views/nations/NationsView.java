8076446606c0425b62bfd1cce17ebad7367eea0b
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-05-25 11:40:30 +0200

    Added title

--- src/main/java/com/application/fumetti/views/nations/NationsView.java
+++ src/main/java/com/application/fumetti/views/nations/NationsView.java
@@ -2,6 +2,7 @@ package com.application.fumetti.views.nations;
 
 import com.application.fumetti.Configuration;
 import com.application.fumetti.views.MainLayout;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -15,5 +16,10 @@ public class NationsView extends VerticalLayout {
     @VaadinServiceScoped
     public NationsView(Configuration config) {
         this.config = config;
+
+        var title = new H1("Nazioni");
+        title.setVisible(true);
+
+        add(title);
     }
 }
