3694bad3104acbc847ed996fc87c0f0987822110
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-05-25 23:29:15 +0200

    Added buttons in dialog

--- src/main/java/com/application/fumetti/views/nations/AddNationDialog.java
+++ src/main/java/com/application/fumetti/views/nations/AddNationDialog.java
@@ -1,13 +1,22 @@
 package com.application.fumetti.views.nations;
 
 import com.application.fumetti.Configuration;
+import com.application.fumetti.mappers.Response;
 import com.application.fumetti.mappers.data.NationResult;
+import com.application.fumetti.mappers.requests.EditorsRequest;
+import com.application.fumetti.utils.Notifications;
+import com.application.fumetti.utils.Requests;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class AddNationDialog extends Dialog {
     private final Configuration config;
@@ -32,7 +41,18 @@ public class AddNationDialog extends Dialog {
     }
 
     private HorizontalLayout initButtons() {
-        return null;
+        var save = new Button("Salva");
+        var cancel = new Button("Annulla", e -> close());
+        save.addClickListener(e -> {
+        });
+
+        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        save.getStyle().set("margin-inline-end", "auto");
+
+        var layout = new HorizontalLayout();
+        layout.add(save, cancel);
+
+        return layout;
     }
 
     public AddNationDialog setGrid(Grid<NationResult> grid) {
