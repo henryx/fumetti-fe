59c69b52157fa31226b55c1f4d22007b99474103
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-05-25 23:27:31 +0200

    Started layout for add nation dialog

--- src/main/java/com/application/fumetti/views/nations/AddNationDialog.java
+++ src/main/java/com/application/fumetti/views/nations/AddNationDialog.java
@@ -3,7 +3,11 @@ package com.application.fumetti.views.nations;
 import com.application.fumetti.Configuration;
 import com.application.fumetti.mappers.data.NationResult;
 import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 public class AddNationDialog extends Dialog {
     private final Configuration config;
@@ -11,6 +15,24 @@ public class AddNationDialog extends Dialog {
 
     public AddNationDialog(Configuration config) {
         this.config = config;
+        var vl = new VerticalLayout();
+        var formLayout = initForm();
+
+        var buttonLayout = this.initButtons();
+
+        vl.setPadding(false);
+        vl.setAlignItems(FlexComponent.Alignment.STRETCH);
+        vl.add(formLayout, buttonLayout);
+
+        add(vl);
+    }
+
+    private FormLayout initForm() {
+        return null;
+    }
+
+    private HorizontalLayout initButtons() {
+        return null;
     }
 
     public AddNationDialog setGrid(Grid<NationResult> grid) {
