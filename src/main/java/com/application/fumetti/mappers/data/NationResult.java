2a967b51675c80954e5fd915be6ade32c0ceec2e
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-05-28 01:37:10 +0200

    Added static methods used to return a mapped record with HashMap data

--- src/main/java/com/application/fumetti/mappers/data/NationResult.java
+++ src/main/java/com/application/fumetti/mappers/data/NationResult.java
@@ -2,8 +2,17 @@ package com.application.fumetti.mappers.data;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.HashMap;
+
 public record NationResult(@JsonProperty("id") Long id,
                            @JsonProperty("name") String name,
                            @JsonProperty("sign") String sign,
                            @JsonProperty("currency") CurrencyResult currency) {
+    public static NationResult map(HashMap<String, Object> data) {
+        var nestedMap = (HashMap<String, Object>) data.get("currency");
+        var currency = CurrencyResult.map(nestedMap);
+
+        return new NationResult(Long.getLong(data.get("id").toString()), data.get("name").toString(),
+                data.get("sign").toString(), currency);
+    }
 }
