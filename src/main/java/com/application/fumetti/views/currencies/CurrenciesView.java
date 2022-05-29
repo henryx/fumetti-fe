e952358c9c52bf848f3b99cd22e6ca9ee0e5e99a
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-05-29 23:13:06 +0200

    Added grid and populated it

--- src/main/java/com/application/fumetti/views/currencies/CurrenciesView.java
+++ src/main/java/com/application/fumetti/views/currencies/CurrenciesView.java
@@ -2,26 +2,22 @@ package com.application.fumetti.views.currencies;
 
 import com.application.fumetti.Configuration;
 import com.application.fumetti.mappers.Response;
-import com.application.fumetti.mappers.data.NationData;
+import com.application.fumetti.mappers.data.CurrencyData;
 import com.application.fumetti.utils.Notifications;
 import com.application.fumetti.utils.Requests;
 import com.application.fumetti.views.MainLayout;
-import com.application.fumetti.views.nations.AddNationDialog;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.renderer.NumberRenderer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.quarkus.annotation.VaadinServiceScoped;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.text.NumberFormat;
 import java.util.HashMap;
 
 @PageTitle("Valuta")
@@ -30,6 +26,7 @@ public class CurrenciesView extends VerticalLayout {
 
     private final Configuration config;
     private final ObjectMapper mapper;
+    private final Grid<CurrencyData> grid;
 
     @VaadinServiceScoped
     public CurrenciesView(Configuration config) {
@@ -40,6 +37,26 @@ public class CurrenciesView extends VerticalLayout {
         var title = new H1("Valuta");
         title.setVisible(true);
 
-        add(title);
+        this.grid = new Grid<CurrencyData>();
+        this.grid.setSelectionMode(Grid.SelectionMode.MULTI);
+        this.grid.addColumn(CurrencyData::id).setHeader("Id").setVisible(false);
+        this.grid.addColumn(CurrencyData::name).setHeader("Nome");
+        this.grid.addColumn(CurrencyData::symbol).setHeader("Simbolo");
+        this.grid.addColumn(new NumberRenderer<>(CurrencyData::valueLire, NumberFormat.getNumberInstance())).setHeader("Valore Lire");
+        this.grid.addColumn(new NumberRenderer<>(CurrencyData::valueEuro, NumberFormat.getNumberInstance())).setHeader("Valore Euro");
+        try {
+            var body = req.get("/currencies");
+            var data = this.mapper.readValue(body, Response.class);
+
+            var currencies = data.getData().stream().map(e -> {
+                var map = (HashMap<String, Object>) e;
+                return CurrencyData.map(map);
+            }).toList();
+            this.grid.setItems(currencies);
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            Notifications.error(e);
+        }
+
+        add(title, this.grid);
     }
 }
