e3c1fcf383a8a7a141bc6f476b3962f019a441ca
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-05-31 00:12:29 +0200

    Added dialog used to add currency

---
+++ src/main/java/com/application/fumetti/views/currencies/AddCurrencyDialog.java
@@ -0,0 +1,113 @@
+package com.application.fumetti.views.currencies;
+
+import com.application.fumetti.Configuration;
+import com.application.fumetti.mappers.Response;
+import com.application.fumetti.mappers.data.CurrencyData;
+import com.application.fumetti.utils.Notifications;
+import com.application.fumetti.utils.Requests;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.NumberField;
+import com.vaadin.flow.component.textfield.TextField;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+
+public class AddCurrencyDialog extends Dialog {
+
+    private final Configuration config;
+    private final ObjectMapper mapper;
+
+    private TextField nameField;
+    private TextField symbolField;
+    private NumberField valueLire;
+    private NumberField valueEuro;
+    private Grid<CurrencyData> grid;
+
+    public AddCurrencyDialog(Configuration config) {
+        this.config = config;
+        this.mapper = new ObjectMapper();
+
+        var vl = new VerticalLayout();
+        var formLayout = initForm();
+
+        var buttonLayout = this.initButtons();
+
+        vl.setPadding(false);
+        vl.setAlignItems(FlexComponent.Alignment.STRETCH);
+        vl.add(formLayout, buttonLayout);
+
+        add(vl);
+    }
+
+    private FormLayout initForm() {
+        var req = new Requests(this.config);
+
+        this.nameField = new TextField("Nome");
+        this.symbolField = new TextField("Simbolo");
+        this.valueLire = new NumberField();
+        this.valueEuro = new NumberField();
+
+        this.valueLire.setLabel("Valore lire");
+        this.valueEuro.setLabel("Valore Euro");
+
+        var layout = new FormLayout();
+        layout.setResponsiveSteps(new FormLayout.ResponsiveStep("0", 2));
+        layout.add(this.nameField, this.symbolField, this.valueLire, this.valueEuro);
+
+        return layout;
+    }
+
+    private HorizontalLayout initButtons() {
+        var save = new Button("Salva");
+        var cancel = new Button("Annulla", e -> close());
+        save.addClickListener(e -> {
+            try {
+                var req = new Requests(this.config);
+                var payload = new CurrencyData(null,
+                        this.nameField.getValue(),
+                        this.symbolField.getValue(),
+                        BigDecimal.valueOf(this.valueLire.getValue()),
+                        BigDecimal.valueOf(this.valueEuro.getValue()));
+                var body = this.mapper.writeValueAsString(payload);
+
+                req.post("/currencies", body);
+
+                var items = req.get("/currencies");
+                var data = this.mapper.readValue(items, Response.class);
+                var currencies = data.getData().stream().map(ie -> {
+                    var map = (HashMap<String, Object>) ie;
+                    return CurrencyData.map(map);
+                }).toList();
+                this.grid.setItems(currencies);
+                this.grid.getDataProvider().refreshAll();
+
+                close();
+            } catch (URISyntaxException | IOException | InterruptedException ex) {
+                Notifications.error(ex);
+            }
+        });
+
+        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        save.getStyle().set("margin-inline-end", "auto");
+
+        var layout = new HorizontalLayout();
+        layout.add(save, cancel);
+
+        return layout;
+    }
+
+    public AddCurrencyDialog setGrid(Grid<CurrencyData> grid) {
+        this.grid = grid;
+        return this;
+    }
+}
