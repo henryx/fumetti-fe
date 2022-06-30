d5eb425ec8a27b2d1690bf335fcfd99284b9f6d0
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-07-01 01:07:58 +0200

    Added record used to map Collection's data

---
+++ src/main/java/com/application/fumetti/mappers/data/CollectionData.java
@@ -0,0 +1,19 @@
+package com.application.fumetti.mappers.data;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.HashMap;
+
+public record CollectionData(@JsonProperty("id") @JsonInclude(JsonInclude.Include.NON_NULL) Long id,
+                             @JsonProperty("name") String name,
+                             @JsonProperty("editor") EditorData editor) {
+
+    public static CollectionData map(HashMap<String, Object> data) {
+        @SuppressWarnings("unchecked") var nestedMap = (HashMap<String, Object>) data.get("editor");
+        var nation = EditorData.map(nestedMap);
+
+        return new CollectionData(Long.getLong(data.get("id").toString()), data.get("name").toString(),
+                nation);
+    }
+}
