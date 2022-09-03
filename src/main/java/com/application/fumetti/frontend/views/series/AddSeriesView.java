99285df3ab3b5b6353d8b5c3752541e4143bfe57
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-09-04 00:47:14 +0200

    Added fields in add series view

--- src/main/java/com/application/fumetti/frontend/views/series/AddSeriesView.java
+++ src/main/java/com/application/fumetti/frontend/views/series/AddSeriesView.java
@@ -1,7 +1,13 @@
 package com.application.fumetti.frontend.views.series;
 
 import com.application.fumetti.frontend.Configuration;
+import com.application.fumetti.frontend.mappers.Response;
+import com.application.fumetti.frontend.mappers.data.EditorData;
+import com.application.fumetti.frontend.utils.Notifications;
+import com.application.fumetti.frontend.utils.Requests;
 import com.application.fumetti.frontend.views.MainLayout;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
@@ -10,27 +16,61 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.List;
+
 @PageTitle("Aggiungi Serie")
 @Route(value = "/series/add", layout = MainLayout.class)
 public class AddSeriesView extends Div {
     private final Configuration config;
+    private final ObjectMapper mapper;
+    private EditorData editorSelected;
 
     public AddSeriesView(Configuration config) {
         this.config = config;
+        this.mapper = new ObjectMapper();
+
         var upper = new VerticalLayout();
+        var center = new VerticalLayout();
         var bottom = new VerticalLayout();
 
         var title = new H2("Aggiungi serie");
+
         var name = new TextField("Nome");
+        var editorCombo = new ComboBox<EditorData>("Editore");
+
+        try {
+            editorCombo.setItems(fetchData("/editors").stream().map(ie -> {
+                var map = (HashMap<String, Object>) ie;
+                return EditorData.map(map);
+            }).toList());
+
+            editorCombo.setItemLabelGenerator(EditorData::name);
+            editorCombo.addValueChangeListener(e -> editorSelected = e.getValue());
+
+        } catch (URISyntaxException | IOException | InterruptedException ex) {
+            Notifications.error(ex);
+        }
 
         upper.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
         upper.setDefaultHorizontalComponentAlignment(FlexComponent.Alignment.CENTER);
-        bottom.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
-        bottom.setDefaultHorizontalComponentAlignment(FlexComponent.Alignment.CENTER);
-        bottom.setAlignItems(FlexComponent.Alignment.STRETCH);
+
+        center.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        center.setDefaultHorizontalComponentAlignment(FlexComponent.Alignment.CENTER);
+        center.setAlignItems(FlexComponent.Alignment.STRETCH);
 
         upper.add(title);
-        bottom.add(name);
-        add(upper, bottom);
+        center.add(name, editorCombo);
+        add(upper, center, bottom);
+    }
+
+    private List fetchData(String endpoint) throws IOException, URISyntaxException, InterruptedException {
+        var req = new Requests(this.config);
+        var body = req.get(endpoint);
+        var data = this.mapper.readValue(body, Response.class);
+
+        return data.getData();
     }
 }
