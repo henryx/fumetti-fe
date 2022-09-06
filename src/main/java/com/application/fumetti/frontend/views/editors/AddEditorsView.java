11cf03e15107e72943599c3e8ef5ed6cd3052b18
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-09-07 00:52:02 +0200

    Started add editor view

---
+++ src/main/java/com/application/fumetti/frontend/views/editors/AddEditorsView.java
@@ -0,0 +1,48 @@
+package com.application.fumetti.frontend.views.editors;
+
+import com.application.fumetti.frontend.Configuration;
+import com.application.fumetti.frontend.views.MainLayout;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+
+@PageTitle("Aggiungi Editore")
+@Route(value = "/editors/add", layout = MainLayout.class)
+public class AddEditorsView extends Div {
+    private final Configuration config;
+    private final ObjectMapper mapper;
+
+    public AddEditorsView(Configuration config) {
+        this.config = config;
+        this.mapper = new ObjectMapper();
+
+        var upper = new VerticalLayout();
+        var center = new VerticalLayout();
+        var bottom = new VerticalLayout();
+
+        bottom.add(this.initButtons());
+
+        add(upper, center, bottom);
+    }
+
+    private HorizontalLayout initButtons() {
+        var save = new Button("Salva");
+        var cancel = new Button("Annulla");
+        cancel.addClickListener(e -> cancel.getUI().ifPresent(ui -> ui.navigate("/editors")));
+        save.addClickListener(e -> {
+        });
+        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        save.getStyle().set("margin-inline-end", "auto");
+
+        var layout = new HorizontalLayout();
+        layout.add(save, cancel);
+
+        return layout;
+    }
+}
