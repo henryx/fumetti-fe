060cb1705c53a55c672674181a3eaa4d8ad23508
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-09-14 00:23:28 +0200

    Added grid used to show series data

--- src/main/java/com/application/fumetti/frontend/views/series/SeriesView.java
+++ src/main/java/com/application/fumetti/frontend/views/series/SeriesView.java
@@ -1,9 +1,16 @@
 package com.application.fumetti.frontend.views.series;
 
 import com.application.fumetti.frontend.Configuration;
+import com.application.fumetti.frontend.mappers.Response;
+import com.application.fumetti.frontend.mappers.data.SeriesData;
+import com.application.fumetti.frontend.utils.Notifications;
+import com.application.fumetti.frontend.utils.Requests;
 import com.application.fumetti.frontend.views.MainLayout;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.icon.Icon;
@@ -15,6 +22,10 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import com.vaadin.quarkus.annotation.VaadinServiceScoped;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+
 @PageTitle("Serie")
 @Route(value = "/series", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
@@ -24,6 +35,7 @@ public class SeriesView extends Div {
     @VaadinServiceScoped
     public SeriesView(Configuration config) {
         this.config = config;
+
         var upper = new VerticalLayout();
         var bottom = new VerticalLayout();
 
@@ -43,7 +55,35 @@ public class SeriesView extends Div {
         bottom.setAlignItems(FlexComponent.Alignment.STRETCH);
 
         upper.add(title);
-        bottom.add(addButton);
+        bottom.add(addButton, initGrid());
         add(upper, bottom);
+    }
+
+    private Grid<SeriesData> initGrid() {
+        var mapper = new ObjectMapper();
+        var req = new Requests(this.config);
+        var grid = new Grid<>(SeriesData.class, false);
+
+        grid.setSelectionMode(Grid.SelectionMode.SINGLE);
+        grid.addColumn(SeriesData::id).setHeader("Id").setVisible(false);
+        grid.addColumn(SeriesData::name).setHeader("Nome");
+        grid.addColumn(cd -> cd.genre().description()).setHeader("Genere");
+        grid.addColumn(cd -> cd.status().description()).setHeader("Stato");
+        grid.addColumn(cd -> cd.frequency().description()).setHeader("Periodicità");
+        grid.addColumn(cd -> cd.editor().name()).setHeader("Editore");
+        try {
+            var body = req.get("/api/v1/series");
+            var data = mapper.readValue(body, Response.class);
+            var series = data.getData().stream().map(e -> {
+                var map = (HashMap<String, Object>) e;
+                return SeriesData.map(map);
+            }).toList();
+            grid.setItems(series);
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            Notifications.error(e);
+        }
+        grid.addThemeVariants(GridVariant.LUMO_WRAP_CELL_CONTENT);
+
+        return grid;
     }
 }
